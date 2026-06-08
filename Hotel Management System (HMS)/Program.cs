ada925ca11d77a41368b711d32ea0535b3e48001
Aliya Alisaii <AliyaRashidSarhan@gmail.com>
2026-06-08 11:40:55 +0400

    create folder

---
+++ Hotel Management System (HMS)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hotel_Management_System__HMS_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
