fa58e2ed83fbf6fcfca1083961745700a050fccb
Aliya Alisaii <AliyaRashidSarhan@gmail.com>
2026-06-08 15:18:01 +0400

    booking service class

--- Hotel Management System (HMS)/Services/BookingService.cs
+++ Hotel Management System (HMS)/Services/BookingService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hotel_Management_System__HMS_.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,41 @@ using System.Threading.Tasks;
 
 namespace Hotel_Management_System__HMS_.Services
 {
-    internal class BookingService
+    public class BookingService
     {
+        public static void DisplayAllBookings(List<BookingModel> ngModel)
+        {
+            foreach (BookingModel booking in ngModel)
+            {
+                Console.WriteLine("Booking Id " + booking.bookingId + "guest Id " + booking.guestId +
+                   " roomNumber " + booking.roomNumber + "status " + booking.status +
+                 "total Price " + booking.totalPrice);
+            }
+        }
+        public static void FindBookingById(List<BookingModel> Model, string bookingId)
+        {
+            foreach (BookingModel booking in Model)
+            {
+                if (booking.bookingId == bookingId)
+                {
+                    return booking;
+                }
+            }
+            return null;
+        }
+        public static bool CancelBooking(BookingModel booking)
+        {
+            if (booking.status == cancelled)
+            {
+                return true;
+            }
+        }
+        public static bool CompleteBooking(BookingModel cbooking)
+        {
+            if (cbooking == null)
+            {
+
+            }
+        }
     }
 }
