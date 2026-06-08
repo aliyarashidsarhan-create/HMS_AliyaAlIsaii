f81919b1ab29c71a1ed480152e79fe5faceea577
Aliya Alisaii <AliyaRashidSarhan@gmail.com>
2026-06-08 15:35:24 +0400

    staff service class

--- Hotel Management System (HMS)/Services/StaffService.cs
+++ Hotel Management System (HMS)/Services/StaffService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hotel_Management_System__HMS_.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,33 @@ using System.Threading.Tasks;
 
 namespace Hotel_Management_System__HMS_.Services
 {
-    internal class StaffService
+    public  class StaffService
     {
+        public static void DisplayAllStaff(List<StaffModel> staffModels)
+        {
+            foreach (StaffModel staff in staffModels)
+            {
+                Console.WriteLine("staff id :"+staff.staffId + "role "+staff.role
+                    + "is On Duty "+staff.isOnDuty);
+            }
+
+
+        }
+        public static int  FindStaffById(List<StaffModel> staffModels, string satffId)
+        {
+            foreach(StaffModel staff in staffModels)
+            {
+                if(staff.staffId == satffId)
+                    return 0;
+            }
+            return null;
+          
+        }
+        public static void ToggleDutyStatus(StaffModel staff)
+        {
+         
+        }
+
+
     }
 }
